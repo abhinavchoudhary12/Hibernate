2d37714e5800c47787d48136de58fa4b77b90110
abhinav <abhinavchoudharygzb1@gmail.com>
2023-08-30 18:03:00 +0530

    learn hql and sql in hibernate

--- com/hql/Hql.java
+++ com/hql/Hql.java
@@ -1,7 +1,11 @@
 package com.hql;
 
-import java.util.List;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,7 +13,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-import com.hib.Hibernate.Student;
+
 
 public class Hql {
 
@@ -17,7 +21,7 @@ public class Hql {
 		// TODO Auto-generated method stub
 		
 		
-		Configuration config=new Configuration().configure("hibernate.cfg.xml");
+		Configuration config=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(HqlDB.class);
 		
 		ServiceRegistry registry=new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
 		
@@ -25,19 +29,88 @@ public class Hql {
 		
 		Session session=factory.openSession();
 		
+		session.beginTransaction();
 		
-		String query="from embeddingname";
+ 		
+
+//		Random r=new Random();
+//		for(int i=1;i<=20;i++) {
+//			HqlDB hb=new HqlDB();
+//			hb.setId(i);
+//			hb.setLanguage("language of random is "+r.nextInt(1000));
+//			hb.setName("name of random is "+r.nextFloat(1000));
+//			session.save(hb);
+//		}
 		
-		System.out.println("he");
-		Query q=session.createQuery(query);
+//		------------getting single result-------------------
 		
-		List<Student> l=q.list();
-		for(Student s:l) {
-			System.out.println(s.getName()+" "+s.getCourse()+" "+s.getId());
+//		Query query=session.createQuery("from HqlDB where id=12");
+//		HqlDB s=(HqlDB)	query.uniqueResult();
+//		System.out.println(s);
 		
+//		------------getting multiple result-----------------
+		
+//		List<HqlDB> list =query.list();
+//		for(HqlDB f:list) {
+//		System.out.println(f);
+//		}
+
+//		-----------------getting result as an array----------------
+		
+//		Query query=session.createQuery("select id,language from HqlDB where id=19");
+//		
+//		Object[] obj=(Object[]) query.uniqueResult();
+//		
+//		System.out.println("id:language");
+//		System.out.println(obj[0]+":"+obj[1]);
+		
+//		----------------------use of aggregation function like sum---------------
+//		Random r=new Random();
+//		for(int i=1;i<=20;i++) {
+//			HqlDB hb=new HqlDB();
+//			hb.setId(i);
+//			hb.setMarks(r.nextInt(100));
+//			hb.setName("name "+i);
+//			session.save(hb);
+//		}
+		
+//		Query query=session.createQuery("select sum(marks) from HqlDB where marks>60");
+//		Long sum=(Long) query.uniqueResult();
+//		System.out.println("sum is "+sum);
+		
+//-----------aggregate function distinct---------------------
+
+//		Query query=session.createQuery("select distinct(marks) from HqlDB where marks>60");
+//		List<Integer> distinct=(List<Integer>) query.list();
+//		for(Integer hb:distinct) 
+//		{
+//		System.out.println(hb);
+//		}
+		
+		
+//		-----------------prepached statement-------------
+//		int max =50;
+//		
+//		Query query=session.createQuery("select name,marks from HqlDB where marks>:max");
+//		query.setParameter("max", max);
+//		List<Object[]> list=(List<Object[]>) query.list(); 
+//		
+//		for(Object[] hb:list) {
+//			System.out.println(hb[0]+" "+hb[1]);
+//		}
+
+		
+//		---------------sql query in hibernate-----------
+		Query query=session.createSQLQuery("select name,marks from HqlDB where marks<=30");
+		query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+		List std=query.list();
+		for(Object o:std)
+		{
+			Map m=(Map)o;
+			System.out.println(m.get("name")+"  :  "+m.get("marks"));
 		}
-		
-		//session.beginTransaction().commit();
+		session.getTransaction().commit();
+		session.close();
 	}
 
 }
